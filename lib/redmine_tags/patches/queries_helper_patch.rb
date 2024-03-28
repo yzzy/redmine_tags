8fced42b48352117d3613fc0a07d4c80bea0870f
yzzy <kyng@lyzard.org>
2024-03-28 16:38:29 +0500

    Update Github actions for Redmine 4.2 again

--- lib/redmine_tags/patches/queries_helper_patch.rb
+++ lib/redmine_tags/patches/queries_helper_patch.rb
@@ -4,6 +4,8 @@ module RedmineTags
       def self.included(base)
         base.send :include, InstanceMethods
         base.class_eval do
+          include TagsHelper
+
           alias_method :column_content_without_redmine_tags, :column_content
           alias_method :column_content, :column_content_with_redmine_tags
 
@@ -13,8 +15,6 @@ module RedmineTags
       end
 
       module InstanceMethods
-        include ::TagsHelper
-
         def column_content_with_redmine_tags(column, issue)
           if column.name == :tags
             column.value(issue).collect{ |t| render_tag_link(t) }
